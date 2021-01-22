b879863489420774ec50892358321a677786ccc3
Lion223 <Lionpower223@gmail.com>
2021-01-22 19:54:36 +0200

    Created buddy class on TaskModel

--- TaskManager.Domain/Entities/TaskModel.cs
+++ TaskManager.Domain/Entities/TaskModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManager.Domain.Entities
 {
@@ -9,7 +8,15 @@ namespace TaskManager.Domain.Entities
     /// <summary>
     // Written the attribute with full name, because just "Table(...)" wasn't working, don't remember, it may work, but don't want to change anything that works at the moment
     [System.ComponentModel.DataAnnotations.Schema.Table("Tasks")]
-    public class TaskModel
+    // Metadata or Buddy class to separate data annotation logic into it's own class
+        // https://stackoverflow.com/questions/4798138/validation-on-my-domain-objects-or-in-the-view-model-where-should-it-go
+        // https://stackoverflow.com/questions/38371519/what-exactly-is-a-buddy-class-and-how-do-i-use-it-to-add-annotations-to-an-exist
+    [MetadataType(typeof(TaskModelMetadata))]
+    public partial class TaskModel
+    {
+    }
+
+    public class TaskModelMetadata
     {
         [ScaffoldColumn(false)]
         [Key]
@@ -19,18 +26,20 @@ namespace TaskManager.Domain.Entities
         public int UserId { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Select the type")]
         public string Type { get; set; }
+        [Required]
+        [Range(0, 3, ErrorMessage = "Select priority on a scale of 0 to 3")]
 
         // Nullable if need to know that user hasn't selected a value
         // So that "Required" attribute will report a validation error (Pro ASP.NET MVC 5, p.37)
         // That also fixes the issue, when SelectTagHelper puts "selected" attribute on int's default value option which is 0
         // Instead of making placeholder selected by default
-        [Required]
-        [Range(0,3, ErrorMessage = "Select priority on a scale of 0 to 3")]
         public int? Priority { get; set; }
         [Required(ErrorMessage = "Select the date for the deadline")]
         public DateTime? Deadline { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Write the description of the task")]
         public string Description { get; set; }
+
+        // Non-nullable for the reason, that by default the task in unfinished, which is intentional
         [ScaffoldColumn(false)]
         [Required]
         public bool Finished { get; set; }
